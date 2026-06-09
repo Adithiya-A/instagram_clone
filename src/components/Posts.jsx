e8edbd22f8b664382a953ccc7127c5f2932c20ae
Adithiya-A <a.adithiya1508@gmail.com>
2026-06-09 21:03:51 +0530

    feat: add-posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState} from 'react'
+import john from '../assets/john.jpg'
+
 
 function Posts() {
 
@@ -12,10 +14,27 @@ function Posts() {
     },[]);
 
   return (
-    <div>
+    <div className="d-flex justify-content-center">
         {posts.length > 0 ?(
             <div>
-                
+                {posts.map((post) => (
+                    <div key = {post.id}>
+                        <div className="d-flex">
+                            <img className="rounded-circle dp" src={post.user.profile_pic} alt={post.user.username}/>
+                            <h5 className="my-auto">{post.user.username}</h5>
+                        </div>
+                        <img src={post.image} alt="post" className="image my-3"/>
+                        <div>
+                            <i className="bi bi-heart"></i>
+                            <i className="bi bi-chat"></i>
+                            <i className="bi bi-send"></i>
+                        </div>
+                        <div>
+                            <b>{post.likes} Likes</b>
+                            <p>{post.caption}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         ):(
             <div>Loading Posts...</div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div className="m-3">
-        <div className="d-flex flex-column gap-3">
-            <img src="src/assets/instatext.png" className="w-50" alt="insta-text" />
+    <div className="m-3 position-fixed">
+        <div className="d-flex flex-column gap-4">
+            <img src="src/assets/instatext.png" className="w-20" alt="insta-text" />
             <div><i className="bi bi-house-door"></i>Home</div>
             <div><i className="bi bi-search"></i>Search</div>
             <div><i className="bi bi-compass"></i>Explore</div>
